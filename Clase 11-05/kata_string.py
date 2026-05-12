9a58ef643c40441e5ba86d696777a0aee3834173
JuanPabloMalizani <jmalizani@frro.utn.edu.ar>
2026-05-11 21:04:44 -0300

    Se realizó el caso de prueba 2, primero dio red y despues green

--- Clase 11-05/kata_string.py
+++ Clase 11-05/kata_string.py
@@ -7,6 +7,10 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string_returns_zero(self):
         self.assertEqual(add(""), 0)
 
+    def test_single_number_returns_value(self):
+        self.assertEqual(add("1"), 1)
+        self.assertEqual(add("5"), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
